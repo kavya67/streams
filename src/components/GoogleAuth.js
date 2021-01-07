1994f328c0e229485e8715bb2bb379d5acbe3bef
kavya <kavya.techmakers@gmail.com>
2021-01-07 17:45:41 +0530

    Added functionality to render authentication status with sign in and sign out buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,47 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null,
+  };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "614727568404-i6oicsuf3m3am13fcqh5etgs965pff60.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "614727568404-i6oicsuf3m3am13fcqh5etgs965pff60.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()})
+  }
+  renderAuthButton() {
+    if(this.state.isSignedIn === null){
+        return null
+    }else if(this.state.isSignedIn){
+        return <button className="ui red google button">
+            <i className="google icon"></i>
+            Sign Out
+        </button>
+    }else {
+        return <button className="ui red google button">
+        <i className="google icon"></i>
+        Sign In with Google
+    </button>
+    }
+}
+
   render() {
-    return <div>Google Auth</div>;
+
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
