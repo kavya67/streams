41745ae2f8ac40fd4c12fc76c509b72ea9c9c1df
kavya <kavya.techmakers@gmail.com>
2021-01-09 00:08:28 +0530

    Displaying validation message

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -5,11 +5,12 @@ import { Field, reduxForm } from "redux-form";
 // reduxForm is a function similar to connect function of react redux
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     ); //Field component returns a object(input)
   }
@@ -21,10 +22,7 @@ class StreamCreate extends Component {
     // console.log(this.props);
     const { handleSubmit } = this.props;
     return (
-      <form
-        className="ui form"
-        onSubmit={handleSubmit(this.onSubmit)}
-      >
+      <form className="ui form" onSubmit={handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
@@ -37,6 +35,20 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You need a title!"; //Field's name property , redux form will match with error object
+  }
+
+  if (!formValues.description) {
+    errors.description = "You need description!";
+  }
+
+  return errors;
+};
 export default reduxForm({
   form: "StreamCreate",
+  validate //hooking up the validate function with redux form
 })(StreamCreate);
