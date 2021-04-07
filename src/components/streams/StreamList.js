dec3254a9d7f3ece9b836d8d09d43577dadafd59
kavya <kavya.techmakers@gmail.com>
2021-04-07 11:43:47 +0530

    Added edit and delete button to stream list based on user id.

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -13,6 +13,7 @@ class StreamList extends Component {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -22,18 +23,32 @@ class StreamList extends Component {
       );
     });
   };
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
       </div>
-    )
+    );
   }
 }
 
 const MapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 
 export default connect(MapStateToProps, { fetchStreams })(StreamList);
