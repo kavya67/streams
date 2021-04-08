ae67f4e9419086542a1ed73d1d14bcb37c0a2b52
kavya <kavya.techmakers@gmail.com>
2021-04-08 19:45:17 +0530

    Added stream form component.

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+//redux form
+import { Field, reduxForm } from "redux-form";
+// Field is a component
+// reduxForm is a function similar to connect function of react redux
+
+
+class StreamForm extends Component {
+  renderError({ error, touched }) {
+    return touched && error ? (
+      <div className="ui error message">
+        <div className="header">{error}</div>
+      </div>
+    ) : (
+      ""
+    );
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        <div>{this.renderError(meta)}</div>
+      </div>
+    ); //Field component returns a object(input)
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+  render() {
+    // console.log(this.props);
+    const { handleSubmit } = this.props;
+    return (
+      <form className="ui form error" onSubmit={handleSubmit(this.onSubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui primary button">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You need a title!"; //Field's name property , redux form will match with error object
+  }
+
+  if (!formValues.description) {
+    errors.description = "You need description!";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "StreamForm",
+  validate, //hooking up the validate function with redux form
+})(StreamForm);
+
