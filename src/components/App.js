57b2c84f9f8f3d9e6d18b1b5ec5eaeb92aebea2f
kavya <kavya.techmakers@gmail.com>
2021-04-09 20:39:28 +0530

    updated stream show component route path.

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ const App = () => {
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/create" exact component={StreamCreate} />
           <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/:id" exact component={StreamShow} />
           <Route path="/streams/delete/:id" exact component={StreamDelete} />
         </div>
       </Router>
